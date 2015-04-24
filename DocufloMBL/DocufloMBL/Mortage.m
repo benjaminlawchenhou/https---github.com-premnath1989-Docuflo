406dca7f54c805c8d7e9bcad728de2955a1a016f
Prem <prem@infoconnect.com.my>
2015-04-24 12:44:17 +0800

    Detail listing changes

--- DocufloMBL/DocufloMBL/Mortage.m
+++ DocufloMBL/DocufloMBL/Mortage.m
@@ -18,18 +18,43 @@
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor colorWithRed:178.0f/255.0f green:177.0f/255.0f blue:177.0f/255.0f alpha:1.0f];
-    UIButton *Close;
-    Close= [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    Close = [[UIButton alloc] initWithFrame:CGRectMake(150, 15, 299, 30)];
-    Close.backgroundColor = [UIColor blackColor];
-    Close.layer.borderColor = [UIColor blackColor].CGColor;
-    Close.layer.borderWidth = 0.5f;
-    Close.layer.cornerRadius = 10.0f;
-    [Close addTarget:self action:@selector(LogOut) forControlEvents:UIControlEventTouchUpInside];
-    [Close setTitle:@"Close" forState:UIControlStateNormal];
-    Close.highlighted = YES;
-    [Close setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [self.view addSubview:Close];
+    
+    UIGraphicsBeginImageContext(CGSizeMake(1900, 1524));
+    //   [drawImage.image drawInRect:CGRectMake(0, 0, 560, 660)];
+    //
+    [[UIImage imageNamed:@"ipad_bg_4.jpg"] drawInRect:self.view.bounds];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    
+    
+    UIGraphicsEndImageContext();
+    
+    self.view.backgroundColor = [UIColor colorWithPatternImage:image];
+    
+//    UIButton *Close;
+//    Close= [UIButton buttonWithType:UIButtonTypeRoundedRect];
+//    Close = [[UIButton alloc] initWithFrame:CGRectMake(150, 15, 299, 30)];
+//    Close.backgroundColor = [UIColor blackColor];
+//    Close.layer.borderColor = [UIColor blackColor].CGColor;
+//    Close.layer.borderWidth = 0.5f;
+//    Close.layer.cornerRadius = 10.0f;
+//    [Close addTarget:self action:@selector(LogOut) forControlEvents:UIControlEventTouchUpInside];
+//    [Close setTitle:@"Close" forState:UIControlStateNormal];
+//    Close.highlighted = YES;
+//    [Close setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+//    [self.view addSubview:Close];
+    
+    
+    UIButton *product= [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    product = [[UIButton alloc] initWithFrame:CGRectMake(480, 0, 50, 60)];
+    UIImage *productImg = [UIImage imageNamed:@"home.png"];
+    [product setImage:productImg forState:UIControlStateNormal];
+    [product addTarget:self action:@selector(LogOut) forControlEvents:UIControlEventTouchUpInside];
+  //  [product setTitle:@"Product" forState:UIControlStateNormal];
+    product.highlighted = YES;
+    [product setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.view addSubview:product];
+
+    
     
     label = [[UILabel alloc] initWithFrame:CGRectMake(15, 15, 299, 30)];
     label.text =@"Image Viewer";
@@ -37,11 +62,11 @@
     label.textColor = [UIColor blackColor];
     label.backgroundColor=[UIColor clearColor];
     [self.view addSubview:label];
-    
-    label = [[UILabel alloc] initWithFrame:CGRectMake(10, 50, 520, 500)];
-    label.textColor = [UIColor blackColor];
-    label.backgroundColor=[UIColor whiteColor];
-    [self.view addSubview:label];
+//    
+//    label = [[UILabel alloc] initWithFrame:CGRectMake(10, 50, 520, 500)];
+//    label.textColor = [UIColor blackColor];
+//    label.backgroundColor=[UIColor whiteColor];
+//    [self.view addSubview:label];
     
     label = [[UILabel alloc] initWithFrame:CGRectMake(11, 50, 515, 30)];
     label.textColor = [UIColor blackColor];
@@ -61,7 +86,7 @@
     label1.text =@" APPLICATION FORM            RECEIVED                     4/22/2015                      WORKFLOW/JACOB ";
     label1.font =[UIFont systemFontOfSize:11];
     label1.textColor = [UIColor whiteColor];
-    label1.backgroundColor=[UIColor grayColor];
+    label1.backgroundColor=[[UIColor grayColor] colorWithAlphaComponent:0.5f];
     [self.view addSubview:label1];
 
     UILabel*label2;
@@ -69,7 +94,7 @@
     label2.text =@" CUSTOMER IC                    RECEIVED                      4/22/2015                       WORKFLOW/JACOB ";
     label2.font =[UIFont systemFontOfSize:11];
     label2.textColor = [UIColor whiteColor];
-    label2.backgroundColor=[UIColor grayColor];
+    label2.backgroundColor=[[UIColor grayColor] colorWithAlphaComponent:0.5f];
     [self.view addSubview:label2];
 
     UILabel*label3;
@@ -77,7 +102,7 @@
     label3.text =@" ID GUARANTOR                   RECEIVED                     4/23/2015                        WORKFLOW/JACOB ";
     label3.font =[UIFont systemFontOfSize:11];
     label3.textColor = [UIColor whiteColor];
-    label3.backgroundColor=[UIColor grayColor];
+    label3.backgroundColor=[[UIColor grayColor] colorWithAlphaComponent:0.5f];
     [self.view addSubview:label3];
     
     UILabel*label4;
@@ -85,7 +110,7 @@
     label4.text =@" APPLICATION FORM            RECEIVED                     4/22/2015                      WORKFLOW/JACOB ";
     label4.font =[UIFont systemFontOfSize:11];
     label4.textColor = [UIColor whiteColor];
-    label4.backgroundColor=[UIColor grayColor];
+    label4.backgroundColor=[[UIColor grayColor] colorWithAlphaComponent:0.5f];
     [self.view addSubview:label4];
     
     UILabel*label5;
@@ -93,7 +118,7 @@
     label5.text =@" CUSTOMER IC                    RECEIVED                      4/22/2015                       WORKFLOW/JACOB ";
     label5.font =[UIFont systemFontOfSize:11];
     label5.textColor = [UIColor whiteColor];
-    label5.backgroundColor=[UIColor grayColor];
+    label5.backgroundColor=[[UIColor grayColor] colorWithAlphaComponent:0.5f];
     [self.view addSubview:label5];
     
     
@@ -102,7 +127,7 @@
     label6.text =@" ID GUARANTOR                   RECEIVED                     4/23/2015                        WORKFLOW/JACOB ";
     label6.font =[UIFont systemFontOfSize:11];
     label6.textColor = [UIColor whiteColor];
-    label6.backgroundColor=[UIColor grayColor];
+    label6.backgroundColor=[[UIColor grayColor] colorWithAlphaComponent:0.5f];
     [self.view addSubview:label6];
     
     UILabel*label7;
@@ -110,7 +135,7 @@
     label7.text =@" CUSTOMER IC                    RECEIVED                      4/22/2015                       WORKFLOW/JACOB ";
     label7.font =[UIFont systemFontOfSize:11];
     label7.textColor = [UIColor whiteColor];
-    label7.backgroundColor=[UIColor grayColor];
+    label7.backgroundColor=[[UIColor grayColor] colorWithAlphaComponent:0.5f];
     [self.view addSubview:label7];
     
     
@@ -119,7 +144,7 @@
     label8.text =@" ID GUARANTOR                   RECEIVED                     4/23/2015                        WORKFLOW/JACOB ";
     label8.font =[UIFont systemFontOfSize:11];
     label8.textColor = [UIColor whiteColor];
-    label8.backgroundColor=[UIColor grayColor];
+    label8.backgroundColor=[[UIColor grayColor] colorWithAlphaComponent:0.5f];
     [self.view addSubview:label8];
 
  
--- DocufloMBL/DocufloMBL/TotalFinance.m
+++ DocufloMBL/DocufloMBL/TotalFinance.m
@@ -16,19 +16,27 @@
 {
     [super viewDidLoad];
     
-    self.view.backgroundColor = [UIColor colorWithRed:178.0f/255.0f green:177.0f/255.0f blue:177.0f/255.0f alpha:1.0f];
-    UIButton *Close;
-    Close= [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    Close = [[UIButton alloc] initWithFrame:CGRectMake(150, 15, 299, 30)];
-    Close.backgroundColor = [UIColor blackColor];
-    Close.layer.borderColor = [UIColor blackColor].CGColor;
-    Close.layer.borderWidth = 0.5f;
-    Close.layer.cornerRadius = 10.0f;
-    [Close addTarget:self action:@selector(LogOut) forControlEvents:UIControlEventTouchUpInside];
-    [Close setTitle:@"Close" forState:UIControlStateNormal];
-    Close.highlighted = YES;
-    [Close setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [self.view addSubview:Close];
+    
+    UIGraphicsBeginImageContext(CGSizeMake(1900, 1524));
+    //   [drawImage.image drawInRect:CGRectMake(0, 0, 560, 660)];
+    //
+    [[UIImage imageNamed:@"ipad_bg_4.jpg"] drawInRect:self.view.bounds];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    
+    
+    UIGraphicsEndImageContext();
+    
+    self.view.backgroundColor = [UIColor colorWithPatternImage:image];
+    
+    UIButton *product= [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    product = [[UIButton alloc] initWithFrame:CGRectMake(480, 0, 50, 60)];
+    UIImage *productImg = [UIImage imageNamed:@"home.png"];
+    [product setImage:productImg forState:UIControlStateNormal];
+    [product addTarget:self action:@selector(LogOut) forControlEvents:UIControlEventTouchUpInside];
+    //  [product setTitle:@"Product" forState:UIControlStateNormal];
+    product.highlighted = YES;
+    [product setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.view addSubview:product];
     
     label = [[UILabel alloc] initWithFrame:CGRectMake(15, 15, 299, 30)];
     label.text =@"Image Viewer";
@@ -36,11 +44,11 @@
     label.textColor = [UIColor blackColor];
     label.backgroundColor=[UIColor clearColor];
     [self.view addSubview:label];
-    
-    label = [[UILabel alloc] initWithFrame:CGRectMake(10, 50, 520, 500)];
-    label.textColor = [UIColor blackColor];
-    label.backgroundColor=[UIColor whiteColor];
-    [self.view addSubview:label];
+    //
+    //    label = [[UILabel alloc] initWithFrame:CGRectMake(10, 50, 520, 500)];
+    //    label.textColor = [UIColor blackColor];
+    //    label.backgroundColor=[UIColor whiteColor];
+    //    [self.view addSubview:label];
     
     label = [[UILabel alloc] initWithFrame:CGRectMake(11, 50, 515, 30)];
     label.textColor = [UIColor blackColor];
@@ -60,7 +68,7 @@
     label1.text =@" APPLICATION FORM            RECEIVED                     4/22/2015                      WORKFLOW/JACOB ";
     label1.font =[UIFont systemFontOfSize:11];
     label1.textColor = [UIColor whiteColor];
-    label1.backgroundColor=[UIColor grayColor];
+    label1.backgroundColor=[[UIColor grayColor] colorWithAlphaComponent:0.5f];
     [self.view addSubview:label1];
     
     UILabel*label2;
@@ -68,7 +76,7 @@
     label2.text =@" CUSTOMER IC                    RECEIVED                      4/22/2015                       WORKFLOW/JACOB ";
     label2.font =[UIFont systemFontOfSize:11];
     label2.textColor = [UIColor whiteColor];
-    label2.backgroundColor=[UIColor grayColor];
+    label2.backgroundColor=[[UIColor grayColor] colorWithAlphaComponent:0.5f];
     [self.view addSubview:label2];
     
     UILabel*label3;
@@ -76,7 +84,7 @@
     label3.text =@" ID GUARANTOR                   RECEIVED                     4/23/2015                        WORKFLOW/JACOB ";
     label3.font =[UIFont systemFontOfSize:11];
     label3.textColor = [UIColor whiteColor];
-    label3.backgroundColor=[UIColor grayColor];
+    label3.backgroundColor=[[UIColor grayColor] colorWithAlphaComponent:0.5f];
     [self.view addSubview:label3];
     
     UILabel*label4;
@@ -84,7 +92,7 @@
     label4.text =@" APPLICATION FORM            RECEIVED                     4/22/2015                      WORKFLOW/JACOB ";
     label4.font =[UIFont systemFontOfSize:11];
     label4.textColor = [UIColor whiteColor];
-    label4.backgroundColor=[UIColor grayColor];
+    label4.backgroundColor=[[UIColor grayColor] colorWithAlphaComponent:0.5f];
     [self.view addSubview:label4];
     
     UILabel*label5;
@@ -92,7 +100,7 @@
     label5.text =@" CUSTOMER IC                    RECEIVED                      4/22/2015                       WORKFLOW/JACOB ";
     label5.font =[UIFont systemFontOfSize:11];
     label5.textColor = [UIColor whiteColor];
-    label5.backgroundColor=[UIColor grayColor];
+    label5.backgroundColor=[[UIColor grayColor] colorWithAlphaComponent:0.5f];
     [self.view addSubview:label5];
     
     
@@ -101,7 +109,7 @@
     label6.text =@" ID GUARANTOR                   RECEIVED                     4/23/2015                        WORKFLOW/JACOB ";
     label6.font =[UIFont systemFontOfSize:11];
     label6.textColor = [UIColor whiteColor];
-    label6.backgroundColor=[UIColor grayColor];
+    label6.backgroundColor=[[UIColor grayColor] colorWithAlphaComponent:0.5f];
     [self.view addSubview:label6];
     
     UILabel*label7;
@@ -109,7 +117,7 @@
     label7.text =@" CUSTOMER IC                    RECEIVED                      4/22/2015                       WORKFLOW/JACOB ";
     label7.font =[UIFont systemFontOfSize:11];
     label7.textColor = [UIColor whiteColor];
-    label7.backgroundColor=[UIColor grayColor];
+    label7.backgroundColor=[[UIColor grayColor] colorWithAlphaComponent:0.5f];
     [self.view addSubview:label7];
     
     
@@ -118,7 +126,7 @@
     label8.text =@" ID GUARANTOR                   RECEIVED                     4/23/2015                        WORKFLOW/JACOB ";
     label8.font =[UIFont systemFontOfSize:11];
     label8.textColor = [UIColor whiteColor];
-    label8.backgroundColor=[UIColor grayColor];
+    label8.backgroundColor=[[UIColor grayColor] colorWithAlphaComponent:0.5f];
     [self.view addSubview:label8];
     
     label1.userInteractionEnabled = YES;
@@ -174,7 +182,7 @@
     
     
     NSUserDefaults *pdfURL = [NSUserDefaults standardUserDefaults];
-    [pdfURL setObject:@"http://192.168.0.103/mobiledoc/imv000001.pdf" forKey:@"URL"];
+    [pdfURL setObject:@"http://192.168.0.103/mobiledoc/inv000001.pdf" forKey:@"URL"];
     
     pdfView *viewController = [[pdfView alloc] init];
     [self presentViewController:viewController animated:YES completion:nil];
@@ -186,7 +194,7 @@
     
     
     NSUserDefaults *pdfURL = [NSUserDefaults standardUserDefaults];
-    [pdfURL setObject:@"http://192.168.0.103/mobiledoc/imv000002.pdf" forKey:@"URL"];
+    [pdfURL setObject:@"http://192.168.0.103/mobiledoc/inv000002.pdf" forKey:@"URL"];
     
     pdfView *viewController = [[pdfView alloc] init];
     [self presentViewController:viewController animated:YES completion:nil];
@@ -198,7 +206,7 @@
     
     
     NSUserDefaults *pdfURL = [NSUserDefaults standardUserDefaults];
-    [pdfURL setObject:@"http://192.168.0.103/mobiledoc/imv000003.pdf" forKey:@"URL"];
+    [pdfURL setObject:@"http://192.168.0.103/mobiledoc/inv000003.pdf" forKey:@"URL"];
     
     pdfView *viewController = [[pdfView alloc] init];
     [self presentViewController:viewController animated:YES completion:nil];
@@ -210,7 +218,7 @@
     
     
     NSUserDefaults *pdfURL = [NSUserDefaults standardUserDefaults];
-    [pdfURL setObject:@"http://192.168.0.103/mobiledoc/imv000004.pdf" forKey:@"URL"];
+    [pdfURL setObject:@"http://192.168.0.103/mobiledoc/inv000004.pdf" forKey:@"URL"];
     
     pdfView *viewController = [[pdfView alloc] init];
     [self presentViewController:viewController animated:YES completion:nil];
@@ -222,7 +230,7 @@
     
     
     NSUserDefaults *pdfURL = [NSUserDefaults standardUserDefaults];
-    [pdfURL setObject:@"http://192.168.0.103/mobiledoc/imv000005.pdf" forKey:@"URL"];
+    [pdfURL setObject:@"http://192.168.0.103/mobiledoc/inv000005.pdf" forKey:@"URL"];
     
     pdfView *viewController = [[pdfView alloc] init];
     [self presentViewController:viewController animated:YES completion:nil];
@@ -234,7 +242,7 @@
     
     
     NSUserDefaults *pdfURL = [NSUserDefaults standardUserDefaults];
-    [pdfURL setObject:@"http://192.168.0.103/mobiledoc/imv000006.pdf" forKey:@"URL"];
+    [pdfURL setObject:@"http://192.168.0.103/mobiledoc/inv000006.pdf" forKey:@"URL"];
     
     pdfView *viewController = [[pdfView alloc] init];
     [self presentViewController:viewController animated:YES completion:nil];
@@ -246,7 +254,7 @@
     
     
     NSUserDefaults *pdfURL = [NSUserDefaults standardUserDefaults];
-    [pdfURL setObject:@"http://192.168.0.103/mobiledoc/imv000007.pdf" forKey:@"URL"];
+    [pdfURL setObject:@"http://192.168.0.103/mobiledoc/inv000007.pdf" forKey:@"URL"];
     
     pdfView *viewController = [[pdfView alloc] init];
     [self presentViewController:viewController animated:YES completion:nil];
@@ -258,7 +266,7 @@
     
     
     NSUserDefaults *pdfURL = [NSUserDefaults standardUserDefaults];
-    [pdfURL setObject:@"http://192.168.0.103/mobiledoc/imv00008.pdf" forKey:@"URL"];
+    [pdfURL setObject:@"http://192.168.0.103/mobiledoc/inv000008.pdf" forKey:@"URL"];
     
     pdfView *viewController = [[pdfView alloc] init];
     [self presentViewController:viewController animated:YES completion:nil];
